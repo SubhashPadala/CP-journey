88f1d64861faa3b328f4d99b17e441d503cf0168
SubhashPadala <111901038@smail.iitpkd.ac.in>
2020-10-28 20:00:16 +0530

    added missing number.cpp

---
+++ CSES_Solutions (S)/Missing Number.cpp
@@ -0,0 +1,49 @@
+/*
+ *     User: eu18
+ *     Problem: Missing Number
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+void solve();
+
+#define FOR(i,a,b) for(int i = a; i < b; ++i)
+#define RFOR(i,a,b) for(int i = a; i >= b; --i)
+#define gcx getchar_unlocked
+#define pcx putchar_unlocked
+#define ll long long
+
+int main()
+{
+    ios_base::sync_with_stdio(false);cin.tie(NULL);
+
+    //#ifndef ONLINE_JUDGE
+    //freopen("inputf.txt", "r", stdin);
+    //freopen("outputf.txt", "w", stdout);
+    //#endif
+
+    int t=1;
+    //cin >> t;
+    while(t--)
+    {
+        solve();
+        cout << "\n";
+    }
+
+    cerr << "time taken : " << (float)clock()/CLOCKS_PER_SEC << " secs" << "\n";
+    return 0;
+}
+
+void solve()
+{
+    ll n, input;
+    cin>>n;
+    ll sum=n*(n+1)/2;
+    n--;
+    while(n--){
+        cin>>input;
+        sum-=input;
+    }
+    cout<<sum;
+    return;
+}

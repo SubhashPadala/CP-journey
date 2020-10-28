25fb5939182500aaf6966d213f4134fcca4c9342
dkodar20 <rudrtiwari@gmail.com>
2020-10-28 16:36:58 +0530

    CSES Solutions folder and solution to Weird Algorithm

---
+++ CSES_Solutions (R)/Weird Algorithm.cpp
@@ -0,0 +1,55 @@
+/*
+ *     User: Rudr Tiwari (HumbleDynamics18 / dkodar)
+ *     Problem: CSES - Weird Algorithm
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+void solve();
+
+#define FOR(i,a,b) for(int i = a; i < b; ++i)
+#define RFOR(i,a,b) for(int i = a; i >= b; --i)
+#define gcx getchar_unlocked
+#define pcx putchar_unlocked
+#define ll long long
+
+int main()
+{
+    ios_base::sync_with_stdio(false);cin.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+    freopen("inputf.txt", "r", stdin);
+    freopen("outputf.txt", "w", stdout);
+    #endif
+
+    int t=1;
+    //cin >> t;
+    while(t--)
+    {
+        solve();
+        cout << "\n";
+    }
+
+    cerr << "time taken : " << (float)clock()/CLOCKS_PER_SEC << " secs" << "\n";
+    return 0;
+}
+
+void solve()
+{	
+	ll n = 0;
+	cin >> n;
+
+	cout << n;
+	while (n != 1){
+
+		cout << " ";
+		if (n%2 == 0)
+			n = n/2;
+
+		else
+			n = 3*n + 1;
+
+		cout << n;
+	}
+
+}

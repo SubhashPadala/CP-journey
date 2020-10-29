68ffcf3f483cc71f36a4078915d59a803c36fe02
SubhashPadala <111901038@smail.iitpkd.ac.in>
2020-10-29 16:05:45 +0530

    added sections and template

---
+++ CSES_Solutions (S)/5. Range Queries/main.cpp
@@ -0,0 +1,35 @@
+/*
+ *     User: eu_18 
+ *     Problem: 
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+void solve();
+
+#define FOR(i,a,b) for(int i = a; i < b; ++i)
+#define RFOR(i,a,b) for(int i = a; i >= b; --i)
+#define gcx getchar_unlocked
+#define pcx putchar_unlocked
+#define ll long long
+int main()
+{
+    ios_base::sync_with_stdio(false);cin.tie(NULL);
+
+
+
+    int t=1;
+    //cin >> t;
+    while(t--)
+    {
+        solve();
+        cout << "\n";
+    }
+
+    cerr << "time taken : " << (float)clock()/CLOCKS_PER_SEC << " secs" << "\n";
+    return 0;
+}
+
+void solve()
+{
+}

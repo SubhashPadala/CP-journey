c2cf57a6524753a43df09965c916535b5a0dc105
dkodar20 <rudrtiwari@gmail.com>
2020-10-31 23:15:56 +0530

    Git corrections :(

---
+++ CodeChef Contests (R)/LunchTime/October LunchTime/Problem1.cpp
@@ -0,0 +1,42 @@
+/*
+ *     User: Rudr Tiwari (HumbleDynamics18 / dkodar)
+ *     Problem: 
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+void solve();
+
+#define FOR(i,a,b) for(int i = a; i < b; ++i)
+#define RFOR(i,a,b) for(int i = a; i >= b; --i)
+#define gcx getchar_unlocked
+#define pcx putchar_unlocked
+#define ll long long
+
+int main()
+{
+    ios_base::sync_with_stdio(false);cin.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+    freopen("inputf.txt", "r", stdin);
+    freopen("outputf.txt", "w", stdout);
+    #endif
+
+    int t=1;
+    cin >> t;
+    while(t--)
+    {
+        solve();
+        cout << "\n";
+    }
+
+    cerr << "time taken : " << (float)clock()/CLOCKS_PER_SEC << " secs" << "\n";
+    return 0;
+}
+
+void solve()
+{
+	long long int x;
+	cin >> x;
+	cout << x << " " << 0;
+}

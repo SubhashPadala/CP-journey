9d98855d002c1737eae08dd47bf66371873a5b49
dkodar20 <rudrtiwari@gmail.com>
2020-11-01 18:43:57 +0530

    Solution to Counting Divisors problem

---
+++ CSES_Solutions (R)/7. Mathematics/Counting Divisors.cpp
@@ -0,0 +1,57 @@
+/*
+ *     User: Rudr Tiwari (HumbleDynamics18 / dkodar)
+ *     Problem: 
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+void solve();
+
+#define FOR(i,a,b) for(int i = a; i < b; ++i)
+#define RFOR(i,a,b) for(int i = a; i >= b; --i)
+#define gcx getchar_unlocked
+#define pcx putchar_unlocked
+#define ll long long
+
+const int MAX_N = 1e5 + 1;
+const int MOD = 1e9 + 7;
+const int INF = 1e9;
+const ll LINF = 1e18;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);cin.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+    freopen("inputf.txt", "r", stdin);
+    freopen("outputf.txt", "w", stdout);
+    #endif
+
+    int t=1;
+    cin >> t;
+    while(t--)
+    {
+        solve();
+        cout << "\n";
+    }
+
+    cerr << "time taken : " << (float)clock()/CLOCKS_PER_SEC << " secs" << "\n";
+    return 0;
+}
+
+void solve()
+{
+	int n;
+	cin >> n;
+
+	int root = int(sqrt(n));
+
+	int ans = 0;
+	FOR(i, 1, root+1)
+		if (n % i == 0)
+			ans += 2;
+
+	if (root*root == n)
+		ans -= 1;
+	cout << ans;
+}

61efee23970e2732942bbe53a79af46f26c752f3
dkodar20 <rudrtiwari@gmail.com>
2020-11-12 20:01:33 +0530

    Solution to Message Route

---
+++ CSES_Solutions (R)/4. Graph Algorithms/Message Route.cpp
@@ -0,0 +1,90 @@
+/*
+ *     User: Rudr Tiwari (HumbleDynamics18 / dkodar)
+ *     Problem: 
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+void solve();
+
+#define FOR(i,a,b) for(int i = a; i < b; ++i)
+#define RFOR(i,a,b) for(int i = a; i >= b; --i)
+#define gcx getchar_unlocked
+#define pcx putchar_unlocked
+#define ll long long
+
+const int MAX_N = 1e5 + 1;
+const int MOD = 1e9 + 7;
+const int INF = 1e9;
+const ll LINF = 1e18;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);cin.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+    freopen("inputf.txt", "r", stdin);
+    freopen("outputf.txt", "w", stdout);
+    #endif
+
+    int t=1;
+    //cin >> t;
+    while(t--)
+    {
+        solve();
+        cout << "\n";
+    }
+
+    cerr << "time taken : " << (float)clock()/CLOCKS_PER_SEC << " secs" << "\n";
+    return 0;
+}
+
+int n, m;
+vector<int> adj[MAX_N], dist;
+int parent[MAX_N];
+
+void bfs(int s) {
+    dist.assign(n + 1, -1);
+    queue<int> q;
+    dist[s] = 0; q.push(s);
+    while (q.size()) {
+        int u = q.front(); q.pop();
+        for (int v : adj[u]) {
+            if (dist[v] == -1) {
+            	parent[v] = u;
+                dist[v] = dist[u] + 1;
+                q.push(v);
+            }
+        }
+    }
+}
+
+void solve() {
+    cin >> n >> m;
+    for (int i = 0; i < m; i++) {
+        int u, v; cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+    bfs(1);
+    if (dist[n] == -1){
+    	cout << "IMPOSSIBLE";
+    	return;
+    }
+
+    cout << dist[n]+1 << "\n";
+
+    int i = n;
+    stack<int> stk;
+    while(i != 0){
+    	stk.push(i);
+    	i = parent[i];
+    }
+
+    while(!stk.empty()){
+    	cout << stk.top() << " ";
+    	stk.pop();
+    }
+
+}
